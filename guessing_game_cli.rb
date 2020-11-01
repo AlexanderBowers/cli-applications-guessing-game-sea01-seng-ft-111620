b83fac4815e72003519a442b5aa9c8dfa2099932
Alexander Bowers <7319abowers@gmail.com>
2020-11-01 09:43:15 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,5 +4,12 @@ def run_guessing_game
   puts "Guess a number between 1 and 6"
   input = gets.chomp
 
-
+  if input == number
+    puts "You guessed the correct number!"
+  elsif input == "exit"
+    puts "Goodbye!"
+    return nil
+  elsif input != number
+    puts "Sorry! The computer guessed #{number}"
+    
 end
